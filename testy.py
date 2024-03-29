b18b7d064fbd001fdc2cb55222589de51129c3e7
harrisonhughes <hkh5485@truman.edu>
2024-03-29 15:53:14 -0500

    Began construction of classroom navigation feature

--- testy.py
+++ testy.py
@@ -81,7 +81,7 @@ paths.append([4, 'translate(468.5,250)', 'M0,0 L0,9 L0,0'])
 paths.append([4, 'translate(508,137.5)', 'M0,0 L14,-14 L0,0'])
 paths.append([4, 'translate(508,235.5)', 'M0,0 L7,7 L14,7 L7,7'])
 paths.append([4, 'translate(448,230)', 'M0,0 L3,3 L3,29 L3,3'])
-paths.append([21, 'translate(380.5,247.5)', 'M0,0 L0,6.5, L0,0'])
+paths.append([21, 'translate(378.5,247.5)', 'M0,0 L0,6.5, L0,0'])
 paths.append([5, 'translate(349,247.5)', 'M0,0 L64,0, L0,0'])
 #Union/Magruder
 paths.append([5, 'translate(3,450)', 'M0,0 L131,0 L0,0'])
@@ -102,10 +102,10 @@ paths.append([10, 'translate(67,546)', 'M0,0 L19,0 L0,0'])
 #Violette/Magruder
 paths.append([6, 'translate(219,25)', 'M0,0 L0,425 L0,0'])
 paths.append([5, 'translate(211,526)', 'M0,0 L-3,26 L-3,60.5'])
-paths.append([8, 'translate(209,526)', 'M0,0 L71,0 L0,0'])
-paths.append([11, 'translate(273,526)', 'M0,0 L20,0 L0,0'])
+paths.append([9, 'translate(209,526)', 'M0,0 L71,0 L0,0'])
+paths.append([15, 'translate(273,528.5)', 'M0,0 L20,0 L0,0'])
 paths.append([5, 'translate(290,526)', 'M0,0 L0,65.72 L0,0'])
-paths.append([5, 'translate(273,452)', 'M0,0 L0,74.5 L0,0'])
+paths.append([7, 'translate(273,452)', 'M0,0 L0,74.5 L0,0'])
 paths.append([5, 'translate(219,452)', 'M0,0 Q5,47,54,74 Q5,47,0,0'])
 paths.append([5, 'translate(213,526)', 'M0,0 Q14,-60,60,-74 Q14,-60,0,0'])
 paths.append([5, 'translate(273,452)', 'M0,0 Q18,25,17,74 Q18,25,0,0'])
@@ -115,7 +115,7 @@ paths.append([5, 'translate(206.5,407)', 'M0,0 L12.5,45 L0,0'])
 paths.append([5, 'translate(216,441)', 'M0,0 Q2.5,15,4,30 Q2.5,15,0,0'])
 paths.append([5, 'translate(219,452)', 'M0,0 Q3,38,-6,74 Q3,38,0,0'])
 paths.append([17, 'translate(341.5,453.38)', 'M0,0 L0,15.62 L0,0'])
-paths.append([13, 'translate(341.5,585)', 'M0,0 L0,10 L0,0'])
+paths.append([13, 'translate(341.5,588)', 'M0,0 L0,7 L0,0'])
 #Union/Lib/Magruder
 paths.append([5, 'translate(203,401)', 'M0,0 L16,-13 L0,0'])
 paths.append([5, 'translate(162,365)', 'M0,0 Q35,8,57,23 Q35,8,0,0'])
@@ -132,7 +132,7 @@ paths.append([5, 'translate(98,315)', 'M0,0 L29,0 L0,0'])
 paths.append([5, 'translate(98,337)', 'M0,0 L29,0 L0,0'])
 #Dobson/Violette
 paths.append([8, 'translate(414,111)', 'M0,0 L0,485.65 L0,0'])
-paths.append([12, 'translate(390,526)', 'M0,0 L24,0 L0,0'])
+paths.append([12, 'translate(390,528.5)', 'M0,0 L24,0 L0,0'])
 paths.append([5, 'translate(414,531.5)', 'M0,0 L69,0 L0,0'])
 paths.append([5, 'translate(414,487)', 'M0,0 L14,0 L0,0'])
 paths.append([5, 'translate(414,576)', 'M0,0 L14,0 L0,0'])
@@ -143,8 +143,8 @@ paths.append([5, 'translate(444,540)', 'M0,0 L-30,27.5 L0,0'])
 paths.append([11, 'translate(284,431)', 'M0,0 L0,21 L0,0'])
 paths.append([13, 'translate(252.5,414)', 'M0,0 L0,38 L0,0'])
 paths.append([5, 'translate(219,430)', 'M0,0 Q10,15,36,22 Q10,15,0,0'])
-paths.append([4, 'translate(339,435)', 'M0,0 L0,17 L0,0'])
-paths.append([4, 'translate(331,435)', 'M0,0 L0,17 L0,0'])
+paths.append([4, 'translate(342,435)', 'M0,0 L0,17 L0,0'])
+paths.append([4, 'translate(335,435)', 'M0,0 L0,17 L0,0'])
 #BNB
 paths.append([5, 'translate(414,363.5)', 'M0,0 L82,0 L0,0'])
 paths.append([5, 'translate(414,295.5)', 'M0,0 L14,0 L0,0'])
@@ -152,27 +152,28 @@ paths.append([5, 'translate(414,431.5)', 'M0,0 L14,0 L0,0'])
 paths.append([5, 'translate(486.5,313.5)', 'M0,0 L0,100, L0,0'])
 paths.append([5, 'translate(414,313.5)', 'M0,0 L72.5,0, L0,0'])
 paths.append([5, 'translate(414,413.5)', 'M0,0 L72.5,0, L0,0'])
-paths.append([5, 'translate(491.5,308.5)', 'M0,0 L-5,5, L0,0'])
-paths.append([5, 'translate(486.5,413.5)', 'M0,0 L5,5, L0,0'])
+paths.append([5, 'translate(489.5,308.5)', 'M0,0 L-3,5, L0,0'])
+paths.append([5, 'translate(486.5,413.5)', 'M0,0 L3,5, L0,0'])
 paths.append([5, 'translate(423,313.5)', 'M0,0 L63.5,100, L0,0'])
 paths.append([5, 'translate(486.5,313.5)', 'M0,0 L-63.5,100, L0,0'])
 paths.append([6, 'translate(150,365)', 'M0,0 L264,0, L0,0'])
 paths.append([4, 'translate(486.5,395)', 'M0,0 L9.5,0, L0,0'])
 paths.append([4, 'translate(486.5,397)', 'M0,0 L-12,0, L0,0'])
 paths.append([4, 'translate(472,259)', 'M0,0 L0,24, L52,24 L0,24' ])
+
 #Health/Kirk
-paths.append([5, 'translate(326,408)', 'M0,0 91,0, L0,0'])
-paths.append([6, 'translate(330,408)', 'M0,0 0,5, L0,0'])
-paths.append([6, 'translate(375,408)', 'M0,0 0,5, L0,0'])
-paths.append([5, 'translate(310,365)', 'M0,0 L0,15 Q0,43,20,43 Q0,41,0,15'])
-paths.append([5, 'translate(349.5,365)', 'M0,0 L0,15 Q0,43,-19.5,43 Q0,41,0,15'])
-paths.append([5, 'translate(354,365)', 'M0,0 L0,15 Q0,43,21,43 Q0,41,0,15'])
-paths.append([5, 'translate(414,365)', 'M0,0 Q-8,40,-34,43 Q-8,40,0,0'])
-paths.append([5, 'translate(354,365)', 'M0,0 Q0,-24,26.5,-24 Q0,-24,0,0'])
-paths.append([5, 'translate(414,365)', 'M0,0 Q0,-24,-34.5,-24 Q0,-24,0,0'])
+paths.append([5, 'translate(330,411)', 'M0,0 84,0, L0,0'])
+paths.append([6, 'translate(330,411)', 'M0,0 0,4, L0,0'])
+paths.append([6, 'translate(375,411)', 'M0,0 0,4, L0,0'])
+paths.append([5, 'translate(310,365)', 'M0,0 L0,15 Q0,43,20,46 Q0,41,0,15'])
+paths.append([5, 'translate(351.5,365)', 'M0,0 L0,15 Q0,43,-21.5,46 Q0,43,0,15'])
+paths.append([5, 'translate(354,365)', 'M0,0 L0,15 Q0,43,21,46 Q0,43,0,15'])
+paths.append([5, 'translate(414,365)', 'M0,0 Q-8,40,-34,46 Q-8,40,0,0'])
+paths.append([5, 'translate(354,365)', 'M0,0 Q0,-24,24.5,-24 Q0,-24,0,0'])
+paths.append([5, 'translate(414,365)', 'M0,0 Q-12,-24,-35.5,-24 Q-12,-24,0,0'])
 paths.append([5, 'translate(349,341)', 'M0,0 L68,0 L0,0'])
 paths.append([5, 'translate(268,365)', 'M0,0 L0,29 L0,0'])
-paths.append([21, 'translate(380.5,336)', 'M0,0 L0,5 L0,0'])
+paths.append([21, 'translate(378.5,336)', 'M0,0 L0,5 L0,0'])
 #Lib/Kirk
 paths.append([5, 'translate(208,283)', 'M0,0 L112,0 L0,0'])
 paths.append([5, 'translate(208,314)', 'M0,0 L11,0 L0,0'])
@@ -199,8 +200,8 @@ km = draw.Path(fill='white', stroke='black', stroke_width='.15', transform='tran
 km.M(0,0).A(14,14,360,1,1,28,0).L(0,0)
 d.append(km)
 
-kirk = draw.Path(fill='white', stroke='black', stroke_width='.15', transform='translate(370,247.5)')
-kirk.M(0,0).A(10.5,10.5,360,1,1,21,0).L(0,0)
+kirk = draw.Path(fill='white', stroke='black', stroke_width='.15', transform='translate(373,247.5)')
+kirk.M(0,0).A(5.5,7.5,360,1,1,11,0).L(0,0)
 d.append(kirk)
 
 for i in range(len(paths)):  
@@ -221,9 +222,9 @@ L87,72 L87,49 L73,49 L73,58 L76,58 L76,136 L74,136 L74,143 L33,143 L33,136 L31,1
 L59,38 L0,38 L0,0 L71,0'''])
 
 #violette
-buildings.append([293, 469, '''M0,0 L17,0 L17,3 L39,3 L39,0 L58,0 L58,3 L80,3 L80,0 L97,0 L97,17 L94,17
-L94,48 L97,48 L97,68 L94,68 L94,99 L97,99 L97,116 L80,116 L80,113 L58,113 L58,116 L39,116 L39,113
-L17,113 L17,116 L0,116 L0,99 L3,99 L3,68 L0,68 L0,48 L3,48 L3,17 L0,17 L0,0 L17,0'''])
+buildings.append([293, 469, '''M0,0 L16,0 L16,1 L39,1 L39,0 L58,0 L58,1 L81,1 L81,0 L97,0 L97,15 L95,15
+L95,49 L97,49 L97,70 L95,70 L95,104 L97,104 L97,119 L81,119 L81,118 L58,118 L58,119 L39,119 L39,118
+L16,118 L16,119 L0,119 L0,104 L2,104 L2,70 L0,70 L0,49 L2,49 L2,15 L0,15 L0,0 L15,0'''])
 
 #magruder
 buildings.append([132, 390, '''M0,0 L9,0 L9,-4 A12,12,360,1,1,18,-4 L18,0 L47,0 L72,0 L72,6 L71,6
@@ -235,20 +236,20 @@ buildings.append([428, 478, '''M0,0 L67,0 L72,5 L72,102 L67,107 L0,107 L0,89 L50
 L0,18 L0,0 L63,0'''])
 
 #bnb
-buildings.append([428, 287, '''M0,0 L28,0 L28,-6 L22,-6 L22,-24 L40,-24 L40,-6 L34,-6 L34,0 L83,0 L85,2
-L85,38 L91.5,38 L91.5,50 L85,50 L85,151 L83,153 L0,153 L0,136 L59,136 L68,127 L68,83 L66,83 L66,71
-L68,71 L68,26 L59,17 L0,17 L0,0 L27,0'''])
+buildings.append([425, 287, '''M0,0 L31,0 L31,-6 L25,-6 L25,-24 L43,-24 L43,-6 L37,-6 L37,0 L82,0 L86,4
+L86,38 L94.5,38 L94.5,50 L86,50 L86,149 L82,153 L0,153 L0,136 L60,136 L69,127 L69,83 L67,83 L67,71
+L69,71 L69,26 L60,17 L0,17 L0,0 L31,0'''])
 
 #missouri
-buildings.append([422, 144, '''M0,0 L14,-13 L38,11 L38,-21 L55,-21 L55,11 L79,-13 L93,0 L69,24 L69,28 L90,28
+buildings.append([420, 144, '''M0,0 L14,-13 L38,11 L38,-21 L55,-21 L55,11 L79,-13 L93,0 L69,24 L69,28 L90,28
 L90,57 69,57 L69,61 L93,85 L79,98 L55,74 L55,106 L38,106 L38,74 L14,98 L0,85 L24,61 L17,55 L17,30 L24,24 L0,0, L14,-13'''])
 
 #power
-buildings.append([239, 394, '''M0,0 L42,0 L42,20 L55,20 L55,42 L42,42 L42,38 L20,38 L20,20 L0,20 L0,0 L42,0'''])
+buildings.append([239, 394, '''M0,0 L44,0 L44,22 L55,22 L55,46 L42,46 L42,42 L20,42 L20,20 L0,20 L0,0 L42,0'''])
 
 #health
-buildings.append([296, 415, '''M0,0 L30,0 L30,-2 L38,-2 L38,0 L75,0 L75,-2 L83,-2 L83,0 L107,0 L107,22
-L46,22 L46,20 L18,20 L18,22 L0,22 L0,0 L28,0'''])
+buildings.append([296, 417, '''M0,0 L30,0 L30,-2 L38,-2 L38,0 L75,0 L75,-2 L83,-2 L83,0 L107,0 L107,21
+L47,21 L47,19 L19,19 L19,21 L0,21 L0,0 L28,0'''])
 
 #library
 buildings.append([135, 183, '''M0,0 L69,0 A6,6,360,0,1,75,6 L75,104 A4.5,4,360,1,1,75,113 L75,175 L65,175
@@ -259,7 +260,7 @@ buildings.append([18, 294, '''M0,0 L70,0 L70,18 L80,18 L80,46 L70,46 L70,107 L39
 L0,39 L-5,39 L-5,25 L0,25 L0,0 L63,0'''])
 
 #kirk
-buildings.append([370, 260, '''M0,0 L0,-6 L21,-6 L21,0 L33,0 L33,70 L21,70 L21,76 L0,76 L0,70 L-12,70 L-12,0 L0,0'''])
+buildings.append([368, 260, '''M0,0 L0,-6 L21,-6 L21,0 L33,0 L33,70 L21,70 L21,76 L0,76 L0,70 L-12,70 L-12,0 L0,0'''])
 
 #memorial
 buildings.append([269, 289, '''M0,0 L14,0 L14,10 L24,10 L24,26 L17,26 L17,36 L-3,36 L-3,26 L-10,26 L-10,10 L0,10 L0,0'''])
@@ -290,7 +291,7 @@ unionLot.M(0,0).L(84,0).L(84,95).L(33,95).L(33,89).L(23,89).L(23,23).L(31,23).L(
 d.append(unionLot)
 
 opLot = draw.Path(fill='white', stroke='black', stroke_width='.3', transform='translate(524.5,45)')
-opLot.M(0,0).L(107.5,0).L(107.5,32).L(117.5,32).L(117.5,47).L(107.5,47).L(107.5,92).L(117.5,92).L(117.5,108).L(107.5,108).L(107.5,151).L(117.5,151).L(117.5,166).L(107.5,166).L(107.5,203).L(0,203).L(0,0).L(104.5,0)
+opLot.M(0,0).L(107.5,0).L(107.5,35).L(117.5,35).L(117.5,50).L(107.5,50).L(107.5,92).L(117.5,92).L(117.5,108).L(107.5,108).L(107.5,151).L(117.5,151).L(117.5,166).L(107.5,166).L(107.5,203).L(0,203).L(0,0).L(104.5,0)
 d.append(opLot)
 
 bnbLot = draw.Path(fill='white', stroke='black', stroke_width='.3', transform='translate(524.5,269)')
@@ -301,19 +302,17 @@ dobLot = draw.Path(fill='white', stroke='black', stroke_width='.3', transform='t
 dobLot.M(0,0).L(37,0).Q(37,-16,48,-16).L(148,-13).L(148,-4).L(55,-6).Q(46,-7,46,0).L(56,0).L(56,81).L(0,81).L(0,0).L(30,0)
 d.append(dobLot)
 
-nodes = {}
-nodes[(205,365)] = ['l-26,0', 'l0,-7', 'l14,0', 'l0,9.55']
 
-#for key in nodes:
-#    for i in nodes[key]:
-#        path = draw.Path(d='M0,0' + i, stroke='navy', stroke_width=2, fill='none', transform='translate(205,365)')
-#        d.append(path)
+for key in nodes:
+    for i in nodes[key]:
+        path = draw.Path(d='M0,0' + i, stroke='navy', stroke_width=2, fill='none', transform='translate' + str(key))
+        d.append(path)
 
-#for key in nodes:
-#    d.append(draw.Circle(0, 0, 2, fill='red', stroke='none', transform='translate(205,365)'))
+for key in nodes:
+    d.append(draw.Circle(0, 0, 2, fill='red', stroke='none', transform='translate' + str(key)))
 
 
-d.set_pixel_scale(4)  # Set number of pixels per geometry unit
+d.set_pixel_scale(2.392)  # Set number of pixels per geometry unit
 #d.set_render_size(400, 200)  # Alternative to set_pixel_scale
 d.save_svg('example.svg')
 d.save_png('example.png')
